32ed2a2808e801e10517284e8a85023af168ca03
Anton <64529246+RexVizsla@users.noreply.github.com>
2023-01-16 09:45:23 +0100

    backup

---
+++ binary.py
@@ -0,0 +1,6 @@
+# The aim of this program is to train converting binary numbers to decimal and the other way around
+# This project uses no external libraries and can therefore be run on python without any further installations
+
+def dec_to_binary(dec):
+    for quot != 0:
+        quot,modulo  = divmod(dec, 2)
